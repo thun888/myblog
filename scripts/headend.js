4e2e66120d78b20f65e100a21aa8b5926d2cdfbf
thun888 <2238342947@qq.com>
2024-02-19 13:11:59 +0800

    C

--- scripts/headend.js
+++ scripts/headend.js
@@ -6,5 +6,62 @@ hexo.extend.injector.register("head_end", function () {
   <link rel="stylesheet" href="https://jsd.onmicrosoft.cn/npm/lxgw-wenkai-screen-webfont@1.1.0/lxgwwenkaiscreen.css" media="all">
   <link rel="stylesheet" href="https://jsd.onmicrosoft.cn/npm/notyf@3/notyf.min.css">
   <link rel="stylesheet" href="/mcss/mcss.css">
+  <style>
+  * {
+      margin: 0;
+      padding: 0;
+  }
+
+  /* From uiverse.io by @satyamchaudharydev */
+  .spinner {
+      width: 100%;
+      min-height: 100vh;
+      background-color: rgb(249, 250, 251);
+  }
+  .loader {
+display: block;
+--height-of-loader: 4px;
+--loader-color: #0071e2;
+width: 130px;
+height: var(--height-of-loader);
+border-radius: 30px;
+background-color: rgba(0,0,0,0.2);
+position: absolute;
+top:50%;
+left: 50%;
+transform: translate(-50%,-50%);
+}
+
+.loader::before {
+content: "";
+position: absolute;
+background: var(--loader-color);
+top: 0;
+left: 0;
+width: 0%;
+height: 100%;
+border-radius: 30px;
+animation: moving 1s ease-in-out infinite;
+;
+}
+
+@keyframes moving {
+50% {
+  width: 100%;
+}
+
+100% {
+  width: 0;
+  right: 0;
+  left: unset;
+}
+}
+</style>
+<script>
+addEventListener("load",function(){
+  document.getElementById("eloading").style.display = "none";
+})
+
+</script>
   `
 })
