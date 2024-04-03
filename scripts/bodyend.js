5cf18facc338df17487d9058a0622d9ad01bfb38
thun888 <2238342947@qq.com>
2024-04-03 20:13:38 +0800

    M theme

--- scripts/bodyend.js
+++ scripts/bodyend.js
@@ -3,6 +3,7 @@
 // code block copy
 hexo.extend.injector.register("body_end", function () {
   return `
+  <script src="/js/extra.js"></script>
   <script defer src="https://seeyou.hzchu.top/justseeyou" data-website-id="9b6818b0-7c75-410b-bde6-40f09d7eca38"></script>
   `
 })
