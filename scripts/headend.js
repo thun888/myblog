1f95d45491485f2ad558846868e83573894f7066
thun888 <2238342947@qq.com>
2024-04-03 21:19:10 +0800

    Change the font link

--- scripts/headend.js
+++ scripts/headend.js
@@ -3,7 +3,7 @@
 // code block copy
 hexo.extend.injector.register("head_end", function () {
   return `
-  <link rel="stylesheet" href="https://jsd.onmicrosoft.cn/npm/lxgw-wenkai-screen-webfont@1.1.0/lxgwwenkaiscreen.css" media="all">
+  <link rel="stylesheet" href="https://jsd.hzchu.top/npm/lxgw-wenkai-screen-webfont@1.1.0/lxgwwenkaiscreen.css" media="all">
   <link rel="stylesheet" href="/mcss/mcss.css">
   <style>
   * {
