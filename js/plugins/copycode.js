4e7765a9c07a5f5a9eeff93769889c305e5f1799
thun888 <thun888@users.noreply.github.com>
2024-05-01 09:28:50 +0000

    deploy: 9a1025cc71a4fc3562c9e0e6b5da1b69a5a4114b

--- js/plugins/copycode.js
+++ js/plugins/copycode.js
@@ -1,32 +1,47 @@
-window.codeElements.forEach((codeElement) => {
-  // 创建复制按钮
-  const codeCopyBtn = document.createElement('div');
-  codeCopyBtn.className = 'copy-btn';
-  codeCopyBtn.textContent = ctx.copycode.default_text;
-  codeElement.appendChild(codeCopyBtn);
+function addCopyButtons(id) {
+  if (id){
+    document.querySelectorAll(`#${id} div tbody tr .code`).forEach((codeElement) => {
+      const codeCopyBtn = document.createElement('div');
+      codeCopyBtn.className = 'copy-btn';
+      codeCopyBtn.textContent = ctx.copycode.default_text;
+      codeElement.appendChild(codeCopyBtn);
+    });
+  }
+  window.codeElements.forEach((codeElement) => {
+    const codeCopyBtn = document.createElement('div');
+    codeCopyBtn.className = 'copy-btn';
+    codeCopyBtn.textContent = ctx.copycode.default_text;
+    codeElement.appendChild(codeCopyBtn);
+  });
+}
 
-  // 添加点击事件监听
-  codeCopyBtn.addEventListener('click', async () => {
-    const codeToCopy = codeElement.querySelector('code')?.textContent || '';
+addCopyButtons()
+
+// 使用事件委托处理复制按钮点击事件
+document.addEventListener('click', async (event) => {
+  if (event.target.classList.contains('copy-btn')) {
+    // 找到复制按钮所在的代码块
+    const codeElement = event.target.closest('.code');
+    const codeToCopy = codeElement.querySelector('pre')?.innerText || '';
     if (navigator.clipboard) {
       try {
         await navigator.clipboard.writeText(codeToCopy);
-        codeCopyBtn.textContent = ctx.copycode.success_text;
-        codeCopyBtn.classList.add('success');
+        event.target.textContent = ctx.copycode.success_text;
+        event.target.classList.add('success');
         hud.toast(ctx.copycode.toast, 2500);
       } catch (error) {
-        codeCopyBtn.textContent = '未获得用户许可';
-        codeCopyBtn.classList.add('warning');
+        event.target.textContent = '未获得用户许可';
+        event.target.classList.add('warning');
       }
     } else {
-      codeCopyBtn.textContent = '浏览器不支持/非HTTPS';
-      codeCopyBtn.classList.add('warning');
+      event.target.textContent = '浏览器不支持/非HTTPS';
+      event.target.classList.add('warning');
     }
 
     // 3秒后恢复默认文本
     setTimeout(() => {
-      codeCopyBtn.textContent = ctx.copycode.default_text;
-      codeCopyBtn.classList.remove('success', 'warning');
+      event.target.textContent = ctx.copycode.default_text;
+      event.target.classList.remove('success', 'warning');
     }, 3000);
-  });
+  }
 });
