27518b94a134645b70effcef89ae865ff7d1afa1
thun888 <2238342947@qq.com>
2024-05-01 19:59:00 +0800

    [M]umami

--- scripts/bodyend.js
+++ scripts/bodyend.js
@@ -4,6 +4,15 @@
 hexo.extend.injector.register("body_end", function () {
   return `
   <script src="/js/extra.js"></script>
-  <script defer src="https://analytics.us.umami.is/script.js" data-website-id="27c6212d-8d91-47a5-ae93-a0fd1bfb0e80"></script>
+  <script>
+  host = window.location.host;
+  if (!host.startsWith("localhost")) {
+    script = document.createElement('script');
+    script.defer = true;
+    script.setAttribute('data-website-id', '27c6212d-8d91-47a5-ae93-a0fd1bfb0e80');
+    script.src = 'https://analytics.us.umami.is/script.js';
+    document.head.appendChild(script);
+  }
+  </script>
   `
 })
