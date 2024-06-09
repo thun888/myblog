d3f8338e8f1d937b438bbbae4a817f41e59d25e5
thun888 <2238342947@qq.com>
2024-06-09 11:24:09 +0800

    use own umami

--- scripts/bodyend.js
+++ scripts/bodyend.js
@@ -4,15 +4,7 @@
 hexo.extend.injector.register("body_end", function () {
   return `
   <script src="/js/extra.js"></script>
-  <script>
-  host = window.location.host;
-  if (!host.startsWith("localhost")) {
-    script = document.createElement('script');
-    script.defer = true;
-    script.setAttribute('data-website-id', '27c6212d-8d91-47a5-ae93-a0fd1bfb0e80');
-    script.src = 'https://analytics.us.umami.is/script.js';
-    document.head.appendChild(script);
-  }
-  </script>
+  <script async defer data-website-id="e2f7364f-2517-4c62-90c8-21a04be3b2e0" src="/js/seeyou.js" data-host-url="https://seeyou.hzchu.top" data-domains="blog.hzchu.top"></script>
+
   `
 })
--- source/wiki/notes/collect/index.md
+++ source/wiki/notes/collect/index.md
@@ -7,3 +7,8 @@ updated: 2024-06-08 12:05:32
 [WSL 双系统端口映射，网络穿透最新教程_wsl端口映射-CSDN博客](https://blog.csdn.net/qq_53679247/article/details/130885858)
 
 [chrome://inspect报404解决方案_chrome inspect 404-CSDN博客](https://blog.csdn.net/xingyu521666/article/details/136127216)
+
+[Umami Docker 部署及优化 · 大大的小蜗牛 (eallion.com)](https://www.eallion.com/umami/)
+
+[Docker 部署 Umami，切换数据库至 PgSQL - ROYWANG](https://roy.wang/docker-umami-to-pgsql/)
+
